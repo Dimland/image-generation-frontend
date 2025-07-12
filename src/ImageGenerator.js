97e3c2df405cbacfc0fe15c528235ab20ceef530
Dimland <dimaq21@gmail.com>
2025-07-13 02:14:43 +0300

    change url

--- src/ImageGenerator.js
+++ src/ImageGenerator.js
@@ -11,27 +11,26 @@ const ImageGenerator = () => {
   const handleGenerateImage = async () => {
     setLoading(true);
     setError('');
-    setImageUrl(''); // Очистка изображения перед новым запросом
+    setImageUrl('');
 
     try {
-      const response = await axios.get('https://backend-container:7079/api/ImageGeneration/generate', {
+      const response = await axios.get('/api/ImageGeneration/generate', {
         params: {
           prompt: prompt,
           width: 1024,
           height: 1024,
           model: 'flux',
         },
-        responseType: 'blob', // Получаем изображение как Blob
+        responseType: 'blob',
       });
 
-      // Преобразуем Blob в base64 через FileReader
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImageUrl(reader.result); // Сохраняем base64 строку
+        setImageUrl(reader.result); 
       };
-      reader.readAsDataURL(response.data); // Читаем как base64
+      reader.readAsDataURL(response.data); 
 
-      setGeneratedPrompt(prompt); // Сохраняем текст, по которому было сгенерировано изображение
+      setGeneratedPrompt(prompt); 
 
     } catch (err) {
       setError('Error during generation');
@@ -56,13 +55,13 @@ const ImageGenerator = () => {
 
       {loading && (
         <div style={{ marginTop: '20px', textAlign: 'center' }}>
-          {/* Локальный GIF с адаптивной шириной */}
+          {}
           <img 
             src="/assets/loading.gif" 
             alt="loading gif"
             style={{
-              width: '50%', // Ограничиваем ширину до 50% от ширины контейнера
-              height: 'auto', // Высота будет пропорциональна ширине
+              width: '50%', 
+              height: 'auto',
             }}
           />
         </div>
